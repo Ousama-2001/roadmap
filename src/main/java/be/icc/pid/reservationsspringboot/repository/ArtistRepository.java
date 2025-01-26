8c2ed957147af4c4de3b35fdc3093bba8bb38a4a
ousama <ousama850@gmail.com>
2025-01-26 01:20:08 +0100

    controller Artist + templates+ routes

--- src/main/java/be/icc/pid/reservationsspringboot/repository/ArtistRepository.java
+++ src/main/java/be/icc/pid/reservationsspringboot/repository/ArtistRepository.java
@@ -1,13 +1,14 @@
 package be.icc.pid.reservationsspringboot.repository;
-
 import java.util.List;
 
 import be.icc.pid.reservationsspringboot.model.Artist;
 import org.springframework.data.repository.CrudRepository;
+
 
 public interface ArtistRepository extends CrudRepository<Artist, Long> {
     List<Artist> findByLastname(String lastname);
 
     Artist findById(long id);
 }
+
 
