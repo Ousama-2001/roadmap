ee4ba4b434cf4bdba703e76d665fae943529dc29
ousama <ousama850@gmail.com>
2025-01-26 22:40:50 +0100

    modele location + relation manyToOne with locality

--- src/main/java/be/icc/pid/reservationsspringboot/model/Locality.java
+++ src/main/java/be/icc/pid/reservationsspringboot/model/Locality.java
@@ -3,16 +3,25 @@ package be.icc.pid.reservationsspringboot.model;
 
 import jakarta.persistence.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
+
+
+
 @Entity
 @Table(name="localities")
 public class Locality {
     @Id
-    @GeneratedValue(strategy= GenerationType.AUTO)
+    @GeneratedValue(strategy=GenerationType.AUTO)
     private Long id;
     private String postalCode;
     private String locality;
 
-    protected Locality() {	}
+    @OneToMany( targetEntity=Location.class, mappedBy="locality" )
+    private List<Location> locations = new ArrayList<>();
+
+    protected Locality() { }
 
     public Locality(String postalCode, String locality) {
         this.postalCode = postalCode;
@@ -37,6 +46,30 @@ public class Locality {
 
     public void setLocality(String locality) {
         this.locality = locality;
+    }
+
+    public List<Location> getLocations() {
+        return locations;
+    }
+
+    public Locality addLocation(Location location) {
+        if(!this.locations.contains(location)) {
+            this.locations.add(location);
+            location.setLocality(this);
+        }
+
+        return this;
+    }
+
+    public Locality removeLocation(Location location) {
+        if(this.locations.contains(location)) {
+            this.locations.remove(location);
+            if(location.getLocality().equals(this)) {
+                location.setLocality(null);
+            }
+        }
+
+        return this;
     }
 
     @Override
