2f2d4faf6166fcc202e94f2b3718dd35ed59622f
ousama <ousama850@gmail.com>
2025-01-27 14:35:14 +0100

    Relation ManyToMany between artist and type

--- src/main/java/be/icc/pid/reservationsspringboot/model/Artist.java
+++ src/main/java/be/icc/pid/reservationsspringboot/model/Artist.java
@@ -1,47 +1,74 @@
 package be.icc.pid.reservationsspringboot.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 @Entity
 @Table(name="artists")
 public class Artist {
     @Id
-    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    @GeneratedValue(strategy= GenerationType.AUTO)
     private Long id;
     private String firstname;
     private String lastname;
 
+    @ManyToMany(mappedBy = "artists")
+    private List<Type> types = new ArrayList<>();
+
     protected Artist() {}
+
     public Artist(String firstname, String lastname) {
         this.firstname = firstname;
         this.lastname = lastname;
     }
+
     public Long getId() {
         return id;
     }
-    public void setId(Long id) {
-        this.id = id;
-    }
+
     public String getFirstname() {
         return firstname;
     }
+
     public void setFirstname(String firstname) {
         this.firstname = firstname;
     }
+
     public String getLastname() {
         return lastname;
     }
+
     public void setLastname(String lastname) {
         this.lastname = lastname;
     }
+
+    public List<Type> getTypes() {
+        return types;
+    }
+
+    public Artist addType(Type type) {
+        if(!this.types.contains(type)) {
+            this.types.add(type);
+            type.addArtist(this);
+        }
+
+        return this;
+    }
+
+    public Artist removeType(Type type) {
+        if(this.types.contains(type)) {
+            this.types.remove(type);
+            type.getArtists().remove(this);
+        }
+
+        return this;
+    }
+
     @Override
     public String toString() {
         return firstname + " " + lastname;
     }
-
 }
