e621d5b6f6227c7a9694ec61436a10ac7d4a603e
ousama <ousama850@gmail.com>
2025-01-27 14:59:20 +0100

    Relation ManyToMany between User and Role

--- src/main/java/be/icc/pid/reservationsspringboot/model/Role.java
+++ src/main/java/be/icc/pid/reservationsspringboot/model/Role.java
@@ -2,18 +2,27 @@ package be.icc.pid.reservationsspringboot.model;
 
 import jakarta.persistence.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Entity
 @Table(name="roles")
 public class Role {
     @Id
-    @GeneratedValue(strategy= GenerationType.AUTO)
+    @GeneratedValue(strategy=GenerationType.AUTO)
     private Long id;
     private String role;
 
-    protected Role() {	}
+    @ManyToMany
+    @JoinTable(
+            name = "user_role",
+            joinColumns = @JoinColumn(name = "role_id"),
+            inverseJoinColumns = @JoinColumn(name = "user_id"))
+    private List<User> users = new ArrayList<>();
+
+    protected Role() { }
 
     public Role(String role) {
-        super();
         this.role = role;
     }
 
@@ -27,6 +36,28 @@ public class Role {
 
     public void setRole(String role) {
         this.role = role;
+    }
+
+    public List<User> getUsers() {
+        return users;
+    }
+
+    public Role addUser(User user) {
+        if(!this.users.contains(user)) {
+            this.users.add(user);
+            user.addRole(this);
+        }
+
+        return this;
+    }
+
+    public Role removeUser(User user) {
+        if(this.users.contains(user)) {
+            this.users.remove(user);
+            user.getRoles().remove(this);
+        }
+
+        return this;
     }
 
     @Override
