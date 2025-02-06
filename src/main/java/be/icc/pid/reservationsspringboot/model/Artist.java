c9445191cec91d863b0de1b32598cd931f7eb959
ousama <ousama850@gmail.com>
2025-02-06 17:37:57 +0100

    Ajout de api et setters

--- src/main/java/be/icc/pid/reservationsspringboot/model/Artist.java
+++ src/main/java/be/icc/pid/reservationsspringboot/model/Artist.java
@@ -28,6 +28,9 @@ public class Artist {
     public Long getId() {
         return id;
     }
+    public void setId(Long id) {
+        this.id = id;
+    }
 
     public String getFirstname() {
         return firstname;
